022b3320ee6f2a821c7f913919855d1050cc11d8
Mallick <mallickr@ES.AD.ADP.COM>
2020-10-14 23:04:08 +0530

    springboot

--- src/main/java/com/test/Sample/SpringBoot/App.java
+++ src/main/java/com/test/Sample/SpringBoot/App.java
@@ -1,5 +1,7 @@
 package com.test.Sample.SpringBoot;
 
+import org.springframework.boot.SpringApplication;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	SpringApplication.run(HelloWorldController.class, args);
     }
 }
